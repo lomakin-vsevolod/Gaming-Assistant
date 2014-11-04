deba17be8143b5bbcc85e1e2900ce2ca2f497568
Vsevolod Lomakin <bloodseva@ya.ru>
2014-11-04 17:48:53 +0300

    Added Project

---
+++ gaming_assistant/app/src/main/java/com/epam/training/gamingassistant/StartActivity.java
@@ -0,0 +1,21 @@
+package com.epam.training.gamingassistant;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class StartActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+
+
+}
